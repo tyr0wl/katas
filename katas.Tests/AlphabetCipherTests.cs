6ec4bd474e1975ab97b8d7e1db3c628e69e561b1
tyr0wl <paul.ch87@gmail.com>
2020-09-26 00:09:01 +0200

    Add failing test

---
+++ katas.Tests/AlphabetCipherTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace katas.Tests
+{
+    public class AlphabetCipherTests
+    {
+        [Test]
+        public void Should_encode_input_with_given_keyword()
+        {
+            // Arrange
+            const string expected = "egsgqwtahuiljgs";
+            var sut = new AlphabetCipher("scones");
+
+            // Act
+            var actual = sut.Encode("meetmebythetree");
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ katas/AlphabetCipher.cs
@@ -0,0 +1,17 @@
+﻿namespace katas
+{
+    public class AlphabetCipher
+    {
+        private readonly string _keyword;
+
+        public AlphabetCipher(string keyword)
+        {
+            _keyword = keyword;
+        }
+
+        public string Encode(string input)
+        {
+            return input;
+        }
+    }
+}
